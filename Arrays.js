83419e62888be11ead749575ad580cf0f9088802
SlavaSt <slavik.st89@gmail.com>
2013-11-02 21:46:55 +0200

    add case class (sortof) support

--- Arrays.js
+++ Arrays.js
@@ -1,17 +1,11 @@
 var d = document;
 
-var Array2 = function(array) {
+function Array2(array) {
 	this.array = array;
-	return new _Array2(array);
+	return CaseClass(Array2, arguments)
 }
 
-var _Array2 = function(array) {
-	this.array = array;
-}
-
-_Array2.prototype = Array2.prototype;
-
-function Arrays() {}
+var Arrays = {};
 
 Arrays.flatten = function(array) {
 	return [].concat.apply([], array);
@@ -42,3 +36,51 @@ if(!Array.isArray) {
     return Object.prototype.toString.call(vArg) === "[object Array]";
   };
 }
+
+if (!String.prototype.trim) {
+String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function defaultFor(arg, val) { 
+  return typeof arg !== 'undefined' ? arg : val; 
+}
+
+// TODO what the fuck is that?
+// This is a constructor that is used to setup inheritance without
+// invoking the base's constructor. It does nothing, so it doesn't
+// create properties on the prototype
+function surrogateCtor() {}
+
+function extend(base, sub) {
+  // Copy the prototype from the base to setup inheritance
+  surrogateCtor.prototype = base.prototype;
+  // Tricky huh?
+  sub.prototype = new surrogateCtor();
+  // Remember the constructor property was set wrong, let's fix it
+  sub.prototype.constructor = sub;
+}
+
+function isUndefined(arg) {
+	return typeof something === "undefined"
+}
+
+function construct(constructor, args) {
+    function F() {
+        return constructor.apply(this, args);
+    }
+    F.prototype = constructor.prototype;
+    return new F();
+}
+
+function CaseClass(superClass, args) {
+	if (CaseClass.flag) {
+		CaseClass.flag = false;
+		return construct(superClass, args);
+	}
+}
+// relying on javascript single threadness. Otherwise would have to use stripe of flags
+CaseClass.flag = true;
